e2d33877add5c49eb49e6a445c1b308ea1b80a8b
Leonardo Tadeu Jaques Steinke <leonardosteinke1@gmail.com>
2020-09-09 02:44:55 -0300

    Update 20200726203436_create_ingrediente_receita.js

--- src/database/migrations/20200726203436_create_ingrediente_receita.js
+++ src/database/migrations/20200726203436_create_ingrediente_receita.js
@@ -3,8 +3,8 @@ exports.up = function (knex) {
     table.increments("id").unique().primary();
     table.integer("id_receita").notNullable();
     table.foreign("id_receita").references("id").inTable("receita");
-    table.integer("id_ingrediente").notNullable();
-    table.foreign("id_ingrediente").references("id").inTable("ingrediente");
+    table.string("ingrediente").notNullable();
+    table.integer("unidade_medida").notNullable();
     table.integer("quantidade").notNullable();
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
