f470820eee9132f7a23faddc18a99c49269fabb9
Leonardo Tadeu Jaques Steinke <leonardosteinke1@gmail.com>
2020-09-17 20:34:33 -0300

    Update 20200827020719_create_usuario_ingrediente.js

--- src/database/migrations/20200827020719_create_usuario_ingrediente.js
+++ src/database/migrations/20200827020719_create_usuario_ingrediente.js
@@ -1,17 +1,16 @@
-
-exports.up = function(knex) {
+exports.up = function (knex) {
     return knex.schema.createTable("usuario_ingrediente", function (table) {
         table.increments("id").unique().primary();
         table.integer("id_secundario").notNullable();
         table.foreign("id_secundario").references("id").inTable("usuario");
-        table.integer("id_ingrediente").notNullable();
-        table.foreign("id_ingrediente").references("id").inTable("ingrediente");
+        table.string("ingrediente").notNullable();
+        table.integer("unidade_medida").notNullable();
         table.integer("quantidade").notNullable();
         table.timestamp('created_at').defaultTo(knex.fn.now());
         table.timestamp('updated_at').defaultTo(knex.fn.now());
-      });
+    });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
     return knex.schema.dropTable("usuario_ingrediente");
 };
